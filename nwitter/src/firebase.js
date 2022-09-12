82fd197aabfc257332335500f49d3c551b1ea4e3
신은정 <ejshin0213@naver.com>
2022-09-12 21:43:49 +0900

    1-0. Firebase Setup

---
+++ nwitter/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2HnTfcUnOgtNt7vaOsNvZTba_x-gbIYE",
+  authDomain: "nwitter-63140.firebaseapp.com",
+  projectId: "nwitter-63140",
+  storageBucket: "nwitter-63140.appspot.com",
+  messagingSenderId: "996672213942",
+  appId: "1:996672213942:web:1df7150d1c1a611bd22717",
+};
+
+export default firebase.initializeApp(firebaseConfig);
