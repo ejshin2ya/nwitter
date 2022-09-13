b769b2ce9378ab9a0e813a9c2498ae6b9f7dd80b
신은정 <ejshin0213@naver.com>
2022-09-13 20:53:06 +0900

    2-1.Login Form part one

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -7,7 +7,7 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />;
+      <AppRouter isLoggedIn={isLoggedIn} />
       <footer>&copy; {new Date().getFullYear}Nwitter</footer>
     </>
   );
